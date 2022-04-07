ed3a0023be003444d994255539cf4bf62e15e229
matcasti <matcasti@umag.cl>
2022-04-07 08:36:44 -0300

    update
    - reviewers notes and answers added to `rmd/*.Rmd` files

--- paper/render-paper.R
+++ paper/render-paper.R
@@ -1,19 +1,25 @@
 if (!requireNamespace("rmarkdown")) install.packages("rmarkdown", dependencies = TRUE)
 
-render_papers = function() {
+render_papers = function(papers = TRUE) {
 
-  files = list.files(
-    path = "paper/rmd/",
-    full.names = TRUE,
-    pattern = ".Rmd$"
-  )
+  files = NULL
+
+  if (papers) {
+    files = list.files(
+      path = "paper/rmd/",
+      full.names = TRUE,
+      pattern = ".Rmd$"
+    )
+  }
+
+  files = c(files, "paper/peer review/author-response.md")
 
   .render = function(input) {
     rmarkdown::render(
       input = input,
       output_dir = "paper/docx/",
       output_format = rmarkdown::word_document(
-        reference_docx = "template.docx"
+        reference_docx = here::here("paper/rmd/template.docx")
       )
     )
   }
